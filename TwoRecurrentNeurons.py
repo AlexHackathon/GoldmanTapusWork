30a1bbcf5d452d81cc924dc91fc5957f0dabab35
AlexHackathon <68239333+AlexHackathon@users.noreply.github.com>
2024-03-11 22:44:21 -0700

    Added comments to TwoRecurrentNeurons

--- TwoRecurrentNeurons.py
+++ TwoRecurrentNeurons.py
@@ -27,22 +27,23 @@ def ConstCurrent(time_vect, stimMag, stimStartEnd_vect):
     return current_vect
 
 
-current1a_vect = ConstCurrent(t_vect, 30, [t_stimStart, t_stimEnd])
-current1b_vect = ConstCurrent(t_vect, 40, [t_stimStart, t_stimEnd])
-weightMatrix = np.array([[0,-0.1],
-                        [-0.1,0]])
-v1a_vect = np.zeros(len(t_vect))
-v1b_vect = np.zeros(len(t_vect))
-tIdx = 1
+current1a_vect = ConstCurrent(t_vect, 30, [t_stimStart, t_stimEnd]) #Current injected into Neuron 1 [spikes/sec]
+current1b_vect = ConstCurrent(t_vect, 40, [t_stimStart, t_stimEnd]) #Current injected into Neuron 2 [spikes/sec]
+weightMatrix = np.array([[0,0],
+                        [0,0]]) #Adjustable weight matrix (-1 to 1 preferrable)
+v1a_vect = np.zeros(len(t_vect)) #Output of Neuron 1 [spikes/sec]
+v1b_vect = np.zeros(len(t_vect)) #Output of Neuron 2 [spikes/sec]
+tIdx = 1 #Time index used for iteration
 
 while tIdx < len(t_vect):
     #Calculating current in Neuron 1
-    u1a_vect = [0,v1b_vect[tIdx-1]]
-    totalInput1a = -v1a_vect[tIdx-1] + current1a_vect[tIdx-1] + sum(np.dot(u1a_vect, weightMatrix))
-    v1a_vect[tIdx] = v1a_vect[tIdx-1] + dt/tau*totalInput1a
+    u1a_vect = [0,v1b_vect[tIdx-1]] #Input of each neuron
+    totalInput1a = -v1a_vect[tIdx-1] + current1a_vect[tIdx-1] + sum(np.dot(u1a_vect, weightMatrix)) #Total input including decay and injected current
+    v1a_vect[tIdx] = v1a_vect[tIdx-1] + dt/tau*totalInput1a #Stepwise calculation of the next firing rate
 
     #Calculating current in Neuron 2
-    u1b_vect = [v1a_vect[tIdx-1],0]
+    #See previous comment
+    u1b_vect = [v1a_vect[tIdx-1],0] 
     totalInput1b = -v1b_vect[tIdx-1] + current1b_vect[tIdx-1] + sum(np.dot(u1b_vect, weightMatrix))
     v1b_vect[tIdx] = v1b_vect[tIdx-1] + dt/tau*totalInput1b
 
