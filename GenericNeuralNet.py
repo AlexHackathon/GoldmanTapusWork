f5c41bdffe92bae247ea7adb9e2bc1b7a0010eb1
AlexHackathon <68239333+AlexHackathon@users.noreply.github.com>
2024-03-11 22:55:09 -0700

    Comments

--- GenericNeuralNet.py
+++ GenericNeuralNet.py
@@ -28,24 +28,32 @@ def ConstCurrent(time_vect, stimMag, stimStartEnd_vect):
 
 class Neuron:
     def __init__(self, current_param, ):
+        '''Sets the current injected over time and initalizes it's firing output vector'''
         self.current_vect = current_param
         self.v_vect = np.zeros(len(t_vect))
     def UpdateRule(self, weights_mat, tIdx_param, u_vect, dt_param, tau_param):
+        '''Uses the linear update rule for multiple neurons'''
         totalInput = -self.v_vect[tIdx_param-1] + self.current_vect[tIdx_param-1] + sum(np.dot(u_vect, weights_mat))
         self.v_vect[tIdx_param] = self.v_vect[tIdx_param-1] + dt_param/tau_param*totalInput
-    def UpdateRuleNonlineardef (self, weights_mat, tIdx_param, u_vect, dt_param, tau_param, activationFunction):
+    def UpdateRuleNonlinear(self, weights_mat, tIdx_param, u_vect, dt_param, tau_param, activationFunction):
+        '''Uses a nonlinear update rule by passing the dot product of input and weights through an activation function'''
         totalInput = -self.v_vect[tIdx_param-1] + self.current_vect[tIdx_param-1] + activationFunction(sum(np.dot(u_vect, weights_mat)))
         self.v_vect[tIdx_param] = self.v_vect[tIdx_param-1] + dt_param/tau_param*totalInput
         
-weightMatrix = np.array([[0,1],
-                        [1,0]])
 tIdx = 1
+#NONFUNCTIONAL
 neuronPop = [] #Array of neurons in the same order as they appear in the weights
-
+#NONFUNCTIONAL
+weightMatrix = np.array([[0,1],
+                        [1,0]]) #Matrix of weights based on the neuron population {len(neuronPop)xlen(neuronPop)}
+#Iterates through time
+#Gets the output of all neurons at the previous time frame
+#Goes through each neuron at a time frame
+#Calculates each neuron's firing rate based on the input
 while tIdx < len(t_vect):
+    u_vect_temp = np.array([])
     for n in neuronPop:
-        u_vect_temp = np.array([])
-        for n in neuronPop:
-            u_vect_temp.append(n.v_vect[tIdx-1])
+        u_vect_temp.append(n.v_vect[tIdx-1])
+    for n in neuronPop:
         n.UpdateRule(weightMatrix, tIdx, u_vect_temp, dt, tau)
     tIdx = tIdx + 1
