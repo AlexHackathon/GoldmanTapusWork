0ae692b302d0dda6ede2bb079928b1d818da6921
AlexHackathon <68239333+AlexHackathon@users.noreply.github.com>
2024-03-18 20:09:03 -0700

    Fitted Linear Regression
    Fit a linear regression model that uses the slope to predict the value of tau. The x values are the delta t since t0 and the y values are the ln(v).

--- AutapseNeuron.py
+++ AutapseNeuron.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from sklearn.linear_model import LinearRegression
 
 dt = 0.1 #Time step [ms]
 t_stimStart = 100 #Time start box current [ms]
@@ -38,7 +39,7 @@ def Activation2(a_param, p_param, r_param):
 
 
 current1_vect = ConstCurrent(t_vect, 30, [t_stimStart, t_stimEnd]) #Current injected into the neuron [spikes/sec]
-weightMatrix = np.array([0])
+weightMatrix = np.array([.5])
 v1a_vect = np.zeros(len(t_vect))
 tIdx = 1
 
@@ -49,13 +50,25 @@ while tIdx < len(t_vect):
     tIdx = tIdx + 1
 
 plt.plot(t_vect, v1a_vect)
+plt.plot(t_stimStart + tau, .63 * 30, 'ro')
 plt.show()
-
-#Nonlinear version
-weightMatrix = np.array([0])
-v1a_vect = np.zeros(len(t_vect))
-tIdx = 1
-while tIdx < len(t_vect):
-    totalInput = np.dot([v1a_vect[tIdx-1]], weightMatrix) + current1_vect[tIdx-1]
-    v1a_vect[tIdx] = v1a_vect[tIdx-1] + dt/tau*(-v1a_vect[tIdx-1] + totalInput)
-    tIdx = tIdx + 1
+#***********************************************************************************
+#Begin linear regression fitting
+timeCalcStart = 550 #Time to start fitting from [ms]
+trainTIdx = np.searchsorted(t_vect, timeCalcStart)
+#trainTIdxEnd = np.searchsorted(t_vect, timeCalcEnd)
+t_cut = t_vect[trainTIdx:] - t_vect[trainTIdx]
+for i in range(0,10):
+    print(t_cut[i])
+lnR_cut = np.log(v1a_vect[trainTIdx:])
+t_cut = t_cut.reshape(-1,1)
+lnR_cut = lnR_cut.reshape(-1,1)
+plt.scatter(t_cut, lnR_cut)
+plt.show()
+reg = LinearRegression()
+reg.fit(t_cut,lnR_cut)
+print(reg.coef_)
+print(reg.intercept_)
+print("Tau is: " + str(-1/reg.coef_))
+print("R0 is: " + str(math.exp(reg.intercept_[0])))
+print("R0 is actually: " + str(v1a_vect[trainTIdx]))
