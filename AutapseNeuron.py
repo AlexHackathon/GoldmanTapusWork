3710338999b7fa6e61f9ac934863b8709c40ed7a
AlexHackathon <68239333+AlexHackathon@users.noreply.github.com>
2024-03-19 10:11:55 -0700

    Plotting Activation Function
    Plotted the activation function r^p/(a+r^p) as a and p vary.

--- AutapseNeuron.py
+++ AutapseNeuron.py
@@ -37,38 +37,74 @@ def Activation2(a_param, p_param, r_param):
     denominator = a_param - r_param
     return numerator/denominator
 
-
-current1_vect = ConstCurrent(t_vect, 30, [t_stimStart, t_stimEnd]) #Current injected into the neuron [spikes/sec]
-weightMatrix = np.array([.5])
-v1a_vect = np.zeros(len(t_vect))
-tIdx = 1
-
-#Linear version
-while tIdx < len(t_vect):
-    totalInput = np.dot([v1a_vect[tIdx-1]], weightMatrix) + current1_vect[tIdx-1]
-    v1a_vect[tIdx] = v1a_vect[tIdx-1] + dt/tau*(-v1a_vect[tIdx-1] + totalInput)
-    tIdx = tIdx + 1
-
-plt.plot(t_vect, v1a_vect)
-plt.plot(t_stimStart + tau, .63 * 30, 'ro')
-plt.show()
-#***********************************************************************************
 #Begin linear regression fitting
+#Global simulation variables that show what to calculate
+plotCurrent = False
+plotFiringRate = False
+plotTeff = False
+plotNonlinear = True
+
+#Variables for linear regression
 timeCalcStart = 550 #Time to start fitting from [ms]
-trainTIdx = np.searchsorted(t_vect, timeCalcStart)
-#trainTIdxEnd = np.searchsorted(t_vect, timeCalcEnd)
-t_cut = t_vect[trainTIdx:] - t_vect[trainTIdx]
-for i in range(0,10):
-    print(t_cut[i])
-lnR_cut = np.log(v1a_vect[trainTIdx:])
-t_cut = t_cut.reshape(-1,1)
-lnR_cut = lnR_cut.reshape(-1,1)
-plt.scatter(t_cut, lnR_cut)
-plt.show()
-reg = LinearRegression()
-reg.fit(t_cut,lnR_cut)
-print(reg.coef_)
-print(reg.intercept_)
-print("Tau is: " + str(-1/reg.coef_))
-print("R0 is: " + str(math.exp(reg.intercept_[0])))
-print("R0 is actually: " + str(v1a_vect[trainTIdx]))
+trainTIdx = np.searchsorted(t_vect, timeCalcStart) #Index where time is greater than timeCalcStart ms
+deltaT = t_vect[trainTIdx:] - t_vect[trainTIdx] #Array of delta ts from t0=timeCalcStart
+deltaT = deltaT.reshape(-1,1)
+
+#Constants for every run
+current1_vect = ConstCurrent(t_vect, 30, [t_stimStart, t_stimEnd]) #Current injected into the neuron [spikes/sec]
+weightValues = np.arange(-1, .99, 0.01)
+t_eff = np.zeros(len(weightValues))
+tauIdx = 0
+
+#Running the simulation for every weight in the weight values
+for w in weightValues:
+    if not plotTeff:
+        break
+    v1a_vect = np.zeros(len(t_vect))
+    tIdx = 1
+    #Update rule for autapse
+    while tIdx < len(t_vect):
+        totalInput = v1a_vect[tIdx-1]*w + current1_vect[tIdx-1]
+        v1a_vect[tIdx] = v1a_vect[tIdx-1] + dt/tau*(-v1a_vect[tIdx-1] + totalInput)
+        tIdx = tIdx + 1
+    #Set the x and y variables for regression including reshaping
+    yReg = np.log(v1a_vect[trainTIdx:])
+    yReg = yReg.reshape(-1,1)
+    #Fit the linear regression
+    reg = LinearRegression()
+    reg.fit(deltaT,yReg)
+    t_eff[tauIdx] = -1/reg.coef_[0][0]
+    tauIdx = tauIdx + 1
+
+#Running a simulation for different values of nonlinear activation functions
+r = np.linspace(0,5, 100)
+a_vals = np.arange(1,10, 2)
+p_vals = np.arange(1,5, 1)
+y_a = np.zeros(len(r))
+
+if plotNonlinear:
+    fig, axs = plt.subplots(ncols=2)
+    fig.suptitle("Activation Function r^p/(a+r^p)")
+
+    for a in a_vals:
+        for i in range(0,len(r)):
+            y_a[i] = r[i]**2/(a+r[i]**2)
+        axs[0].plot(r, y_a, label=str(a))
+    axs[0].set_title("Varrying a Values With p=2")
+    axs[0].legend()
+
+    for p in p_vals:
+        for i in range(0,len(r)):
+            y_a[i] = r[i]**p/(2+r[i]**p)
+        axs[1].plot(r, y_a, label=str(p))
+    axs[1].set_title("Varrying p Values With a=2")
+    axs[1].legend()
+
+
+    if plotTeff:
+        plt.plot(weightValues, t_eff)
+    plt.show()
+
+#Running a simulation using a given nonlinear activation function
+
+
